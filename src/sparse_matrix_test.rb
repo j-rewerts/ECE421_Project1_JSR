a82ef076cb22c2aca2c3a03a5b4fc5c4c444cc60
suhussai <suhussai@ualberta.ca>
2016-01-26 16:47:33 -0700

    add comments, and test for trace

--- src/sparse_matrix_test.rb
+++ src/sparse_matrix_test.rb
@@ -17,6 +17,7 @@ class SparseMatrixTest < Test::Unit::TestCase
     end
 
     def test_transpose
+      # http://matrix.reshish.com/transCalculation.php
       # assign 2d array to make into sparse matrices
       m1 = [[1,0,2],[0,3,0]] # transpose of m1
       m2 = [[1,0],[0,3],[2,0]] # transpose of m2
@@ -35,6 +36,7 @@ class SparseMatrixTest < Test::Unit::TestCase
     end
 
     def test_size
+      # https://en.wikipedia.org/wiki/Matrix_%28mathematics%29#Size
       # assign 2d array to make into sparse matrices
       m1 = [[1,0,2],[0,3,0]] # transpose of m1
       m2 = [[1,0],[0,3],[2,0]] # transpose of m2
@@ -43,11 +45,34 @@ class SparseMatrixTest < Test::Unit::TestCase
       sparse_m1 = SparseMatrix.new(m1)
       sparse_m2 = SparseMatrix.new(m2)
 
+      # get sizes
       m1_size = sparse_m1.size()
       m2_size = sparse_m2.size()
 
+      # check sizes
       assert(m1_size == [2,3])
       assert(m2_size == [3,2])
     end
-    
+
+
+    def test_trace
+      # http://matrix.reshish.com/transCalculation.php
+      # assign 2d array to make into sparse matrices
+      m1 = [[1,0,2],[0,3,0],[1,0,1]] # transpose of m1
+      m2 = [[1,0],[0,3],[2,0]] # transpose of m2
+
+      # get corresponding sparse matrix objects
+      sparse_m1 = SparseMatrix.new(m1)
+      sparse_m2 = SparseMatrix.new(m2)
+
+      # calculate traces
+      sparse_m1_trace = sparse_m1.trace()
+      sparse_m2_trace = sparse_m2.trace()
+
+      # check for correct trace
+      assert(sparse_m2_trace == 1 + 3 + 1) # sum of diagonals
+      assert_not_equal(sparse_m1_trace, sparse_m1)
+      assert(sparse_m1_trace == -1) # bad number or exception?
+    end
+
 end
