175e42f1f512015945655900fb2dc056e1c11c93
Jared Rewerts <rewerts@ualberta.ca>
2016-01-26 16:00:12 -0700

    Added the add test.

--- src/sparse_matrix_test.rb
+++ src/sparse_matrix_test.rb
@@ -1,10 +1,13 @@
 require "test/unit"
+require "matrix"
 require_relative "sparse_matrix"
 
 class SparseMatrixTest < Test::Unit::TestCase
 
     def test_add
-        
+        array1 = Matrix[[7, 9, 11], [6, 8, 10], [5, 7, 9]]
+        array2 = Matrix[[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+        result = array1 + array2
 
         assert(SparseMatrix.new(array1).add(array2).equals(result));
     end
