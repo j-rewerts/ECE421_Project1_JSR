29a9aa471ba254ae276569a10a0c8ead2f7b311f
Jared Rewerts <rewerts@ualberta.ca>
2016-01-26 15:21:50 -0700

    Stubbed out most of the functions for our sparse matrix library.

---
+++ src/sparse_matrix.rb
@@ -0,0 +1,69 @@
+
+class SparseMatrix
+
+	def add(array)
+
+	end
+
+	def subtract(array)
+
+	end
+
+	def matrix_multiply(array)
+
+	end
+
+	# This function may not need to exist. It could just be part of the other multiply function
+	def scalar_multiply(value)
+
+	end
+
+	def elementwise_multiply(array)
+
+	end
+
+	def invert()
+
+	end
+
+	def transpose()
+
+	end
+
+	def get_rank()
+
+	end
+
+	def is_invertable()
+
+	end
+
+	def get_trace()
+
+	end
+
+	def determinant()
+
+	end
+
+	def is_empty()
+
+	end
+
+	# Array*Array^T=Identity=Array^T*Array
+	def is_orthagonal()
+
+	end
+
+	def is_square()
+
+	end
+
+	def is_singular()
+
+	end
+
+	def equals(array)
+
+	end
+end
