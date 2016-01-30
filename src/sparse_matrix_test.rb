01adb0e326fa8cb7453e163a5dfa301484a7b8e1
suhussai <suhussai@ualberta.ca>
2016-01-30 11:50:25 -0700

    add pre/post conditions & and invars for transpose test

--- src/sparse_matrix_test.rb
+++ src/sparse_matrix_test.rb
@@ -50,18 +50,33 @@ class SparseMatrixTest < Test::Unit::TestCase
       # assign 2d array to make into sparse matrices
       m1 = [[1,0,2],[0,3,0]] # transpose of m1
       m2 = [[1,0],[0,3],[2,0]] # transpose of m2
+      m3 = [[0,0],[0,0]]
 
       # get corresponding sparse matrix objects
       sparse_m1 = SparseMatrix.new(m1)
       sparse_m2 = SparseMatrix.new(m2)
+      sparse_m3 = SparseMatrix.new(m3)
+
+      # pre-conditions & invariants
+      assert(sparse_m1.is_a SparseMatrix)
+      assert(sparse_m2.is_a SparseMatrix)
+      assert(sparse_m3.is_a SparseMatrix)
+
 
       # calculate transposes
       sparse_m1_transposed = sparse_m1.transpose()
       sparse_m2_transposed = sparse_m2.transpose()
+      sparse_m3_transposed = sparse_m3.transpose()
 
-      # check for correct transposition (?)
+      # post-conditions & invariants
+      assert(sparse_m1.is_a SparseMatrix)
+      assert(sparse_m2.is_a SparseMatrix)
+      assert(sparse_m3.is_a SparseMatrix)
+
       assert(sparse_m1_transposed == sparse_m2)
       assert(sparse_m2_transposed == sparse_m1)
+      assert(sparse_m3_transposed == sparse_m3)
+
     end
 
     def test_size
